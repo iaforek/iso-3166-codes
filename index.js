37eb1e03b934db3f6b4b558adedd82e8a2218bfd
iaforek <arkadiusz@jaworski.pl>
2021-12-06 10:52:03 +0100

    Replaced mocha with jest and added code coverage

--- index.js
+++ index.js
@@ -6,19 +6,19 @@
 /**
  * Country codes scraped from https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2
  */
-const countryCodes = require('./country-codes') 
+const countryCodes = require('./country-codes');
 
 /**
  * Get country name for given ISO 3166 country code
- * 
+ *
  * @param {String} code ISO 3166 two character country code
  * @returns {String} Country Name
  * @throws {Error}
  */
 function getCountryName(code) {
-    const country = countryCodes[code];
-    if (!country) throw Error('Country not found');
-    return country;
+  const country = countryCodes[code];
+  if (!country) throw Error('Country not found');
+  return country;
 }
 
-module.exports = { getCountryName }
+module.exports = { getCountryName };
